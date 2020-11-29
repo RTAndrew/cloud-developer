ad0f6cdd883e2743649935af462c9131198c1a4c
RTAndrew <anderson_rodax@hotmail.com>
2020-11-29 21:24:54 +0100

    chore: rename unrelated variables

--- final-project/backend/src/businessLogic/attachmentUrl.ts
+++ final-project/backend/src/businessLogic/attachmentUrl.ts
@@ -4,10 +4,10 @@ import AttachmentsStorage from '../dataLayer/attachmentsStorage'
 
 const attachmentStorage = new AttachmentsStorage()
 
-export async function getAttachmentUrl(todoId: string): Promise<string> {
-    return await attachmentStorage.getAttachmentUrl(todoId)
+export async function getAttachmentUrl(photoId: string): Promise<string> {
+    return await attachmentStorage.getAttachmentUrl(photoId)
 }
 
-export function getPresignedUrl(todoId: string): string | null {
-    return attachmentStorage.getPresignedUrl(todoId)
+export function getPresignedUrl(photoId: string): string | null {
+    return attachmentStorage.getPresignedUrl(photoId)
 }
--- final-project/backend/src/lambda/http/updatePhoto.ts
+++ final-project/backend/src/lambda/http/updatePhoto.ts
@@ -9,12 +9,12 @@ import { updatePhoto, getPhotoById } from '../../businessLogic/photos'
 import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
 
-const logger = createLogger('updateTodo')
+const logger = createLogger('updatePhoto')
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
   const photoId = event.pathParameters.photoId
-  const updatedTodo: UpdatePhotoRequest = JSON.parse(event.body)
+  const updatedPhoto: UpdatePhotoRequest = JSON.parse(event.body)
   const userId = getUserId(event)
 
   const result = await getPhotoById(userId, photoId)
@@ -27,12 +27,12 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
-      body: JSON.stringify(`Todo not exists.`)
+      body: JSON.stringify(`Photo not exists.`)
     }
   }
 
-  logger.info(`User ${userId} updating todo: ${photoId} with data ${updatedTodo}`)
-  await updatePhoto(userId, photoId, updatedTodo)
+  logger.info(`User ${userId} updating photo: ${photoId} with data ${updatedPhoto}`)
+  await updatePhoto(userId, photoId, updatedPhoto)
 
   return {
     statusCode: 200,
